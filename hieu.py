5d1409028c8484640c387a37e1c33a646318a27b
khoa.trandang2 <khoa.trandang2@di4l.vn>
2024-11-28 10:25:17 +0700

    first commit

---
+++ hieu.py
@@ -0,0 +1,12 @@
+you = "hello"
+
+if you == "":
+    robot_brain = "I can't hear you, try again"
+elif you == "hello":
+    robot_brain = "Hello Dang Khoa"
+elif you == "today":
+    robot_brain = "thu 4"
+else:
+    robot_brain = "I'm fine thank you, and you"
+
+print(robot_brain)
